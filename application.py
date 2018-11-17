f265f54feb32f6b2ecfe6b43a17111954e1fbd4f
Jonathan Tshimpaka <jonathantshimpaka@Jonathans-MacBook-Air.local>
2018-11-17 14:10:44 -0500

    all changes

--- application.py
+++ application.py
@@ -15,9 +15,18 @@ app.config["SESSION_TYPE"] = "filesystem"
 socketio = SocketIO(app)
 
 message_id = 0
+
+#list of all the users
 usernames = []
+
+#list of all chatrooms
 channel_names = []
+
+#all messags containted in the chatrooms
 channel_contents = {}
+
+#maps users to channels that are members, or channel they can access
+users_toChannels = {}
 
 
 def insertIt(theList, item):
@@ -57,15 +66,15 @@ def index():
     """
         Go to the homapege when the user first get to the webpage
     """
-    return render_template("index.html", channels=channel_names)
+    return render_template("index.html")
 
-@app.route("/home")
-def home():
+@app.route("/home/<username>")
+def home(username):
     """
         When the user goes back to the homapage
     """
-    return jsonify({"channels": channel_names})
-
+    print("We on the homepage")
+    return jsonify({"channels": users_toChannels[username]})
 
 @app.route("/signup", methods=["POST"])
 def signup():
@@ -73,59 +82,71 @@ def signup():
         Allow the user to create an unique display name(aka username)
         Make sure the user picked an unique display name that does not exist yet
     """
+    global  usernames
     username = request.form.get("username")
-    print(username)
+    print("the username ", username)
+    print("all usernames: ", usernames)
     if username in usernames:
         return jsonify({"success":False})
     else:
         insertIt(usernames, username)
+        users_toChannels[username] = []
         return jsonify({"success":True})
 
-@app.route("/create_channel", methods=["GET", "POST"])
+@app.route("/create_channel_post", methods=["POST"])
 def create_channel():
     """
         Allow the user to create a channel as long as the name of the channel
         name has not been taken yet"""
 
-    if request.method == "POST":
-        channel = request.form.get("channel-name").lower()
-        if channel in channel_names:
-            return jsonify({"failure":True})
+    channel = request.form.get("channel-name").lower()
+    founder = request.form.get("founder")
+    if channel in channel_names:
+        return jsonify({"failure":True})
 
-        else:
-            insertIt(channel_names, channel)
-            channel_contents[channel] = []
-            print("this is channel content", channel_contents)
-            return jsonify({"failure":False})
     else:
-        return render_template("index.html", channels=channel_names)
+        insertIt(channel_names, channel)
+        channel_contents[channel] = [{"next_messageId": 0, "members":[founder], "founder":founder}]
+        insertIt(users_toChannels[founder], channel)
+        print("this is channel content", channel_contents)
+        return jsonify({"failure":False})
 
+@app.route("/create_channel/<username>")
+def go_create_channel(username):
+    return render_template("index.html", channels=users_toChannels[username])
 
-
-
-
-@app.route("/channel/json/<channel_name>")
-def channel_page(channel_name):
+@app.route("/channel/json/<channel_name>/<username>")
+def channel_page(channel_name, username):
     """
         Display the channel content(messages)
     """
-    return jsonify({"messages":channel_contents[channel_name]})
+    if channel_name in  users_toChannels[username]:
+        return jsonify({"messages":channel_contents[channel_name][1:],"channels": users_toChannels[username]})
+
+    else:
+        return redirect(url_for("home", username=username))
+
+
+
+
 
 @app.route("/channel/<channel_name>")
 def channel(channel_name):
     """
         Display the channel content. This is when the user refresh the webpage
     """
-    return render_template("index.html", messages=channel_contents[channel_name], channels=channel_names)
+    return render_template("index.html", messages=channel_contents[channel_name][1:], channels=channel_names)
 
 
 @socketio.on("sending message")
-def vote(data):
+def send_message(data):
     """
         allow the user to send a message, and other user to receive inside a channel
         add message to the channel
     """
     global message_id
+
+    print("ibside the socket blablabla")
     message = data["message"]
     username = data["username"]
     current_time = data["current_time"]
@@ -133,7 +154,7 @@ def vote(data):
     date = data["date"]
 
     #check if it is the first message of the day or not
-    if len(channel_contents[channel]) == 0:
+    if len(channel_contents[channel]) == 1:
         emit("broadcast message", {"date":date, "message":message, "username":username, "current_time":current_time, "channel":channel, "id":message_id}, broadcast=True)
     else:
         if channel_contents[channel][-1]["date"] != date:
@@ -146,15 +167,6 @@ def vote(data):
     #increment the message_id variable
     message_id += 1
 
-@socketio.on("create channel")
-def new_channel(data):
-    """
-        add new channel to the navbar of every user
-    """
-    channel = data["channel"]
-    user = data["user"]
-    emit("broadcast new_channel", {"channel":channel, "user": user}, broadcast=True)
-
 @app.route("/deleteMessage/<id>/<channel>")
 def delete_message(id, channel):
     """
@@ -162,14 +174,79 @@ def delete_message(id, channel):
     """
     id=int(id)
     messageDate =""
-    for message in channel_contents[channel]:
+    print(channel_contents[channel][1:])
+
+    #remove the message was the channel
+    for message in channel_contents[channel][1:]:
         if message["id"] == id:
             messageDate = message["date"]
             channel_contents[channel].remove(message)
-    if len(channel_contents[channel]) == 0:
-        print("it is zero")
+
+    #check if this was the only message of the day
+    if len(channel_contents[channel]) == 1:
         return jsonify({"removedate": True})
+
     if channel_contents[channel][-1]["date"] != messageDate:
-        print("they are not equal")
         return jsonify({"removedate": True})
     return jsonify({})
+
+
+@app.route("/json/<channel>/add_user/<username>")
+def usernames_list(channel, username):
+    """
+        render list of usernames that can be added to the chatroom
+    """
+    copy = usernames[:]
+    result = []
+
+    #lis of all members
+    members = channel_contents[channel][0]["members"]
+    length = len(copy)
+
+    for i in range(0, length):
+        if copy[i] not in members:
+            result.append(copy[i])
+    return jsonify({"usernames":result})
+
+
+
+@socketio.on("add user")
+def add_user(data):
+    """
+       Allow the founder of the channel to add new user to their channel
+    """
+    channel = data["channel"]
+    user = data["user"]
+    founder = data["founder"]
+
+
+    insertIt(channel_contents[channel][0]["members"], user)
+    insertIt(users_toChannels[user], channel)
+
+    socketio.emit('broadcast added_user', {'founder':founder, 'user': user, 'channel':channel}, broadcast=True)
+
+
+@app.route("/<channel>/members")
+def members(channel):
+    """
+        return list of all the members in the chatrooms
+    """
+    print("we supposed to be in here")
+    return jsonify({"members":channel_contents[channel][0]["members"]})
+
+
+
+
+@socketio.on("remove user")
+def remove_user(data):
+    """
+       Allow the founder of the channel to remove user from their channel
+    """
+    channel = data["channel"]
+    user = data["user"]
+    founder = data["founder"]
+
+    channel_contents[channel][0]["members"].remove(user)
+    users_toChannels[user].remove(channel)
+
+    socketio.emit('broadcast remove_user', {'founder':founder, 'user': user, 'channel':channel}, broadcast=True)
